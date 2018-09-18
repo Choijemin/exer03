7a6d150e947a1f2459dc353a79e25708cec026ba
User <User@DESKTOP-ER5TABD>
2018-09-18 15:12:05 +0900

    어려운 커밋

---
+++ src/controllers/LoginController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import models.AccountDao;
+
+@WebServlet("/login.do")
+public class LoginController extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		String id = req.getParameter("id");
+		String pass = req.getParameter("pass");
+		
+		AccountDao acd = new AccountDao();
+		Map m = acd.loginck(id);
+		
+		System.out.println(m);
+		
+		if(session.getAttribute("id") == m) {
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/home.jsp");
+			rd.forward(req, resp);
+			
+		}  else {
+			RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/login.jsp");
+			rd.forward(req, resp);
+		}
+	}
+}
---
+++ src/models/AccountDao.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import oracle.net.ns.SessionAtts;
+
+	
+
+public class AccountDao {
+	SqlSessionFactory factory;
+	
+	public AccountDao() throws IOException{
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+		factory = builder.build(is);
+	}
+	public int addAccount(Map map) {
+		SqlSession sql = factory.openSession();
+		try {
+			int r = sql.insert("account.addUsingMap", map);
+			if(r == 1) 
+				sql.commit();
+			return r;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return -1;
+		}	 
+	}
+	public Map loginck(String id) {
+		SqlSession sql = factory.openSession();
+		try {
+			return sql.selectOne("account.loginck", id);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
